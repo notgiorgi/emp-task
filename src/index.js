4f71434e466d10514456301642a18fcf94a1a730
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-04-14 23:58:48 +0400

    Added semantic css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+
+import '../node_modules/semantic-ui-css/semantic.min.css'
 import './index.css'
 
 ReactDOM.render(
