444639bf7952756a5f0f9c1479ee9e884ddc8f47
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-04-14 15:43:10 +0400

    Initial project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react'
 
+import {
+  SearchBar,
+  Results,
+  Auth,
+} from './features'
+
+import {
+  Layout,
+  Main,
+  Sidebar,
+} from './views'
+
 class App extends Component {
   render() {
-    return <div>Hi</div>
+    return (
+      <Layout>
+        <Main>
+          <SearchBar />
+          <Results />
+        </Main>
+
+        <Sidebar>
+          <Auth />
+        </Sidebar>
+      </Layout>
+    )
   }
 }
 
---
+++ src/features/index.js
@@ -0,0 +1,3 @@
+export { SearchBar } from './SearchBar'
+export { Auth } from './Auth'
+export { Results } from './Results'
